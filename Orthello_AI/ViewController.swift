e7c52297ab564f03344c89218f5eb08ed16d9c6c
Fredrik Bixo <fredrik.bixo@gmail.com>
2020-07-19 12:38:02 +0200

    Initial Commit

---
+++ Orthello_AI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Orthello_AI
+//
+//  Created by Fredrik Bixo on 2020-07-19.
+//  Copyright © 2020 Fredrik Bixo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Orthello_AITests/Orthello_AITests.swift
@@ -0,0 +1,34 @@
+//
+//  Orthello_AITests.swift
+//  Orthello_AITests
+//
+//  Created by Fredrik Bixo on 2020-07-19.
+//  Copyright © 2020 Fredrik Bixo. All rights reserved.
+//
+
+import XCTest
+@testable import Orthello_AI
+
+class Orthello_AITests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
